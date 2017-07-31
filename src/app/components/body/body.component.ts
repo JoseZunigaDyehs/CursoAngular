c972bf24c816e8c45da4e9d5a07e5ea3e95947cf
JoseZunigaDyehs <zuniga.joseignacio@gmail.com>
2017-07-30 23:51:40 -0400

    Initial Commit

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html'
+})
+export class BodyComponent {
+  mostrar:boolean=false;
+
+  frase:any = {
+    mensaje:"Porque correr el riesgo, nunca es lo que vende...",
+    autor: "Hordatoj"
+  }
+
+  personaje:string[] = ['Nacho','Jose','Joseuaño','Asw','Dyehs'];
+}
